c679b0490b8c6f60b5fe8a52f0b78421fc5e0981
Jxstxs <juleb0202@gmail.com>
2022-11-02 19:31:35 +0100

    fix: removed make command before valgrind

--- source/commands.zsh
+++ source/commands.zsh
@@ -8,4 +8,5 @@ function pacsi() {
 alias srcall="source $HOME/.zshrc"
 alias update_xres="xrdb -merge ~/.Xresources; xsetroot -name 'fsignal:reloadXresources'; pkill -USR1 st"
 
-alias valcheck="make >/dev/null 2>&1 && sudo DEBUGINFOD_URLS="https://debuginfod.archlinux.org" G_SLICE=always-malloc valgrind"
+# alias valcheck="make >/dev/null 2>&1 && sudo DEBUGINFOD_URLS="https://debuginfod.archlinux.org" G_SLICE=always-malloc valgrind"
+alias valcheck="sudo DEBUGINFOD_URLS="https://debuginfod.archlinux.org" G_SLICE=always-malloc valgrind"
